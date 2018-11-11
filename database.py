404f663dfee4a22ceb53788d6220f218d36af0cf
PolyKen <kennyxz@live.cn>
2018-11-11 19:22:17 +0800

    update

--- database.py
+++ database.py
@@ -1,16 +1,17 @@
-import MySQLdb as mysql
+import pymysql 
 
 
 def test_database():
-    conn = mysql.connect(host="localhost", user="", passwd="", db="vtransaction")                                     
-    cursor = conn.cursor()
-    cursor.execute("select * from users;")                
-    results = cursor.fetchall()                                                             
-    for row in results:                                                                     
-        id = row[0]                                                                    
-        username = row[1]
-        password = row[2]
-        print(id, username, password)
-                                                                                            
-    cursor.close()                                                                           
-    conn.close()
+    conn = pymysql.connect(host="localhost", user="", passwd="", db="vtransaction")
+    try:
+        with conn.cursor() as cursor:
+            cursor = conn.cursor()
+            cursor.execute("select * from users;")
+            results = cursor.fetchall()
+            for row in results:
+                id = row[0]
+                username = row[1]
+                password = row[2]
+                print(id, username, password)
+    finally:
+        conn.close()
