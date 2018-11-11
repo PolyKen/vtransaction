d81a295619c7e6f5ea46302d68f4e09d5d5056ea
PolyKen <kennyxz@live.cn>
2018-11-11 19:15:04 +0800

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from database import test_database
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test')
+def test():
+    try:
+        test_database()
+    except Exception as e:
+        print(e)
+        return str(e)
+    return 'success'
 
 
 if __name__ == '__main__':
