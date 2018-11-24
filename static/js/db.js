ac56c2b25d6729bc1f487b077dba4686c959c7a6
PolyKen <kennyxz@live.cn>
2018-11-24 14:36:14 +0800

    update

--- static/js/db.js
+++ static/js/db.js
@@ -25,17 +25,22 @@ function parse_obj(obj_string) {
     //{'user': 'test_user', 'id': 3, 'price': 3.0, 'time…datetime(2018, 11, 14, 21, 1, 10), 'quantity': 2}
     let user_ptn = /'user': '.*?'/;
     let user = obj_string.match(user_ptn)[0].slice(9, -1);
-    console.log(user);
+    console.log("user:", user);
     let id_ptn = /'id': .*?,/;
     let id = obj_string.match(id_ptn)[0].slice(6, -1);
+    id = Number(id);
+    console.log("id:", id);
     let price_ptn = /'price': .*?,/;
     let price = obj_string.match(price_ptn)[0].slice(9, -1);
+    price = Number(price);
+    console.log("price:", price);
     let time_ptn = /datetime(.*)/;
     let time = obj_string.match(time_ptn)[0].slice(9, -1);
-    console.log(time);
+    console.log("time:", time);
     let quantity_ptn = /'quantity': .*}/;
     let quantity = obj_string.match(quantity_ptn)[0].slice(12, -1);
-    console.log(quantity);
+    quantity = Number(quantity);
+    console.log("quantity:", quantity);
 }
 
 function read_transaction() {
