d68f8c677ab709dab963dbd3d361f1d80df81984
PolyKen <kennyxz@live.cn>
2018-11-24 20:30:56 +0800

    update

--- static/js/db.js
+++ static/js/db.js
@@ -12,7 +12,7 @@ function read_wish() {
 }
 
 function parse_wish_table(raw_data) {
-    let obj_pattern = /"(.*?)"/g;
+    let obj_pattern = /\"(.*?)\"/g;
     let raw_obj_list = raw_data.match(obj_pattern);
     let obj_list = [];
     for (let i = 0; i < raw_obj_list.length; i++) {
