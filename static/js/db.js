fe7b7c00a6b872d0df6e1142e396d21156c34371
PolyKen <kennyxz@live.cn>
2018-11-24 14:53:04 +0800

    update

--- static/js/db.js
+++ static/js/db.js
@@ -1,10 +1,8 @@
 function read_wish() {
     $.get("/read-wish/buy", function (data) {
-        console.log(data);
         console.log(parse_wish_table(data));
     });
     $.get("/read-wish/sell", function (data) {
-        console.log(data);
         console.log(parse_wish_table(data));
     });
 }
@@ -19,28 +17,39 @@ function parse_wish_table(raw_data) {
         console.log(obj);
         obj_list.push(obj);
     }
+    return obj_list;
 }
 
 function parse_obj(obj_string) {
+    let obj = Object();
     //{'user': 'test_user', 'id': 3, 'price': 3.0, 'time…datetime(2018, 11, 14, 21, 1, 10), 'quantity': 2}
     let user_ptn = /'user': '.*?'/;
     let user = obj_string.match(user_ptn)[0].slice(9, -1);
-    console.log("user:", user);
+    obj.user = user;
     let id_ptn = /'id': .*?[,}]/;
     let id = obj_string.match(id_ptn)[0].slice(6, -1);
     id = Number(id);
-    console.log("id:", id);
+    obj.id = id;
     let price_ptn = /'price': .*?[,}]/;
     let price = obj_string.match(price_ptn)[0].slice(9, -1);
     price = Number(price);
-    console.log("price:", price);
+    obj.price = price;
     let time_ptn = /datetime(.*)/;
     let time = obj_string.match(time_ptn)[0].slice(9, -1);
-    console.log("time:", time);
+    let raw_array = time.match(/\d+[,)]/g);
+    let dt_array = [];
+    for (let i = 0; i < raw_array.length; i++) {
+        t = raw_array[i];
+        t = t.slice(0, -1);
+        dt_array.push(Number(t));
+    }
+    obj.time = dt_array;
     let quantity_ptn = /'quantity': .*?[,}]/;
     let quantity = obj_string.match(quantity_ptn)[0].slice(12, -1);
     quantity = Number(quantity);
-    console.log("quantity:", quantity);
+    obj.quantity = quantity;
+
+    return obj;
 }
 
 function read_transaction() {
