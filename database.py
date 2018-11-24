476cf8a7e6b3e37d29a0b00dc4cd8bda17cfae2f
PolyKen <kennyxz@live.cn>
2018-11-24 18:00:54 +0800

    update

--- database.py
+++ database.py
@@ -114,7 +114,8 @@ def read_latest_wish():
     try:
         with conn.cursor() as cursor:
             cursor.execute("select * from wish order by id desc limit 1;")
-            result = cursor.fetchall()[0]
+            result = cursor.fetchall()
+            print(result)
     except Exception as e:
         print(e)
         conn.close()
